6b759ff924102bb3e1197ba87aea93548568fbf9
giomen88 <giorgia9288@virgilio.it>
2022-06-27 23:38:34 +0200

    milestone3 da finire

--- js/script.js
+++ js/script.js
@@ -1,11 +1,54 @@
+// RECUPERO GALLERIA DAL DOM
 const gallery = document.querySelector('.gallery');
 
-const images = ["img/0.jpg", "img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"]
+// CREO ARRAY RISORSE
+const images = ["img/0.jpg", "img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
 
-let imgList = '';
+// VARIABILE + CICLO FOR + MANIPOLAZIONE PER INSERIRE IMMAGINI NEL DOM
+let image = '';
 
 for (let i = 0; i < images.length; i++) {
-    imgList += `<img src="${images[i]}">`
+    image += `<img src="${images[i]}" alt="image${[i]}">`;
 }
 
-gallery.innerHTML = imgList
+gallery.innerHTML = image;
+
+// RECUPERO BOTTONI DAL DOM
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+// RECUPERO IL TAG IMG NEL DOM
+const element = document.getElementsByTagName('img');
+
+// VARIABILE PER IMMAGINE ATTIVA
+let currentActiveIndex = 0;
+
+// AGGIUNGO CLASSE ACTIVE ALLA PRIMA IMMAGINE
+element[currentActiveIndex].classList.add('active');
+
+/////////////////////////// CLICK BOTTONE NEXT
+
+// AGGIUNGO FUNZIONE AL BUTTON NEXT
+
+nextButton.addEventListener('click', function () {
+    element[currentActiveIndex].classList.remove('active');
+
+    currentActiveIndex++;
+
+    element[currentActiveIndex].classList.add('active');
+
+})
+
+/////////////////////////// CLICK BOTTONE PREV
+
+// AGGIUNGO FUNZIONE AL BUTTON PREV
+prevButton.addEventListener('click', function () {
+
+    element[currentActiveIndex].classList.remove('active');
+
+    currentActiveIndex--;
+
+    element[currentActiveIndex].classList.add('active');
+
+})
+
