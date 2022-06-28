0f7743403c52038e60c0a6751a9df4a7a3945813
giomen88 <giorgia9288@virgilio.it>
2022-06-28 17:49:25 +0200

    correzioni + modifiche + bonus

--- js/script.js
+++ js/script.js
@@ -1,30 +1,36 @@
-// RECUPERO GALLERIA DAL DOM
+// RECUPERO GALLERIA E THUMBNAILS DAL DOM
 const gallery = document.querySelector('.gallery');
+const thumbnails = document.querySelector('.thumbnails')
 
 // CREO ARRAY RISORSE
-const images = ["img/0.jpg", "img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
+const sources = ["img/0.jpg", "img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
 
 // VARIABILE + CICLO FOR + MANIPOLAZIONE PER INSERIRE IMMAGINI NEL DOM
-let image = '';
+let galleryImages = '';
+let thumbnailsImages = '';
 
-for (let i = 0; i < images.length; i++) {
-    image += `<img src="${images[i]}" alt="image${[i]}">`;
+for (let i = 0; i < sources.length; i++) {
+    galleryImages += `<img src="${sources[i]}" alt="gallery-image${[i]}">`;
+    thumbnailsImages += `<img src="${sources[i]}" alt="thumbnails-image${[i]}">`;
 }
 
-gallery.innerHTML = image;
+gallery.innerHTML = galleryImages;
+thumbnails.innerHTML = thumbnailsImages;
 
 // RECUPERO BOTTONI DAL DOM
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
 
 // RECUPERO IL TAG IMG NEL DOM
-const element = document.getElementsByTagName('img');
+const galleryElements = document.querySelectorAll('.gallery img');
+const thumbnailsElements = document.querySelectorAll('.thumbnails img');
 
 // VARIABILE PER IMMAGINE ATTIVA
 let currentActiveIndex = 0;
 
 // AGGIUNGO CLASSE ACTIVE ALLA PRIMA IMMAGINE
-element[currentActiveIndex].classList.add('active');
+galleryElements[currentActiveIndex].classList.add('active');
+thumbnailsElements[currentActiveIndex].classList.add('active');
 
 /////////////////////////// CLICK BOTTONE NEXT
 
@@ -32,15 +38,17 @@ element[currentActiveIndex].classList.add('active');
 
 nextButton.addEventListener('click', function () {
 
-    element[currentActiveIndex].classList.remove('active');
+    galleryElements[currentActiveIndex].classList.remove('active');
+    thumbnailsElements[currentActiveIndex].classList.remove('active');
 
     currentActiveIndex++;
 
-    if (currentActiveIndex === element.length) {
+    if (currentActiveIndex === galleryElements.length && thumbnailsElements.length) {
         currentActiveIndex = 0;
     }
 
-    element[currentActiveIndex].classList.add('active');
+    galleryElements[currentActiveIndex].classList.add('active');
+    thumbnailsElements[currentActiveIndex].classList.add('active');
 })
 
 /////////////////////////// CLICK BOTTONE PREV
@@ -48,14 +56,16 @@ nextButton.addEventListener('click', function () {
 // AGGIUNGO FUNZIONE AL BUTTON PREV
 prevButton.addEventListener('click', function () {
 
-    element[currentActiveIndex].classList.remove('active');
+    galleryElements[currentActiveIndex].classList.remove('active');
+    thumbnailsElements[currentActiveIndex].classList.remove('active');
 
     currentActiveIndex--;
 
     if (currentActiveIndex < 0) {
-        currentActiveIndex = element.length - 1;
+        currentActiveIndex = galleryElements.length - 1 && thumbnailsElements.length - 1;
     }
 
-    element[currentActiveIndex].classList.add('active');
+    galleryElements[currentActiveIndex].classList.add('active');
+    thumbnailsElements[currentActiveIndex].classList.add('active');
 })
 
